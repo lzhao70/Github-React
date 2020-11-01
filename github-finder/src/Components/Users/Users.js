e1cf8e9e4d84127b8f788e3cdc63509772b42a76
Lucio Zhao <lucio.zhao12@gmail.com>
2020-11-01 16:46:33 -0500

    adjust placement of search component

--- github-finder/src/Components/Users/Users.js
+++ github-finder/src/Components/Users/Users.js
@@ -6,11 +6,12 @@ import Search from '../Search/Search';
 const Users = (props) => {
   return (
     <div class='container'>
+      <Search searchUsers={props.searchUsers} />
+
       {props.isLoading ? (
         <Spinner />
       ) : (
         <div>
-          <Search searchUsers={props.searchUsers} />
           <div className='user-card-container'>
             {props.users.map((user) => (
               <User user={user} />
