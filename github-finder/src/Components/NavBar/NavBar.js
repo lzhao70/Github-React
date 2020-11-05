66c29d64c0ebf56eee0b862fe3eb5d2c8cd02272
Lucio Zhao <lucio.zhao12@gmail.com>
2020-11-05 18:31:31 -0500

    fix nav and cards

--- github-finder/src/Components/NavBar/NavBar.js
+++ github-finder/src/Components/NavBar/NavBar.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const NavBar = ({ title }) => {
   return (
     <div className='nav bg-dark text-dark'>
       <nav className='nav-bar nav-container'>
         <h1 className='nav-title'>{title}</h1>
+        <ul className='nav-links'>
+          <li>
+            <Link className='nav-link' to='/'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className='nav-link' to='/about'>
+              About
+            </Link>
+          </li>
+        </ul>
       </nav>
     </div>
   );
