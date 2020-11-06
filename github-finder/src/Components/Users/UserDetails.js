90626eae692b86782a85286377731cdf263782d4
Lucio Zhao <lucio.zhao12@gmail.com>
2020-11-05 19:27:55 -0500

    add user details form

--- github-finder/src/Components/Users/UserDetails.js
+++ github-finder/src/Components/Users/UserDetails.js
@@ -6,7 +6,39 @@ class UserDetails extends Component {
   }
 
   render() {
-    return <div>{this.props.user.login}</div>;
+    return (
+      <div className='user-card detail-card'>
+        <div className='detail-card-row'>
+          <label htmlFor=''>Login</label>
+          <input type='text' value={this.props.user.login} disabled />
+        </div>
+
+        <div className='detail-card-row'>
+          <label htmlFor=''>Full Name</label>
+          <input type='text' value={this.props.user.name} disabled />
+        </div>
+
+        <div className='detail-card-row'>
+          <label htmlFor=''>Followers</label>
+          <input type='text' value={this.props.user.followers} disabled />
+        </div>
+
+        <div className='detail-card-row'>
+          <label htmlFor=''>Location</label>
+          <input type='text' value={this.props.user.location} disabled />
+        </div>
+
+        <div className='detail-card-row'>
+          <label htmlFor=''>Followers</label>
+          <input type='text' value={this.props.user.followers} disabled />
+        </div>
+
+        <div className='detail-card-row'>
+          <label htmlFor=''>Following</label>
+          <input type='text' value={this.props.user.following} disabled />
+        </div>
+      </div>
+    );
   }
 }
 
